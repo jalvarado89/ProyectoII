68c3e14cd020270edbc586e1a775de6f292decf4
jalvarado89 <tpizo@hotmail.com>
2014-11-26 11:40:37 -0600

    archivo eliminado

--- app/models/usuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class usuario extends Eloquent
-{
-	protected $table = 'usuario';
-	protected $fillable = array('nombre', 'nombre_usuario' , 'contrasena');
-	protected $guarded  = array('id');
-	public    $timestamps = false;
-}
